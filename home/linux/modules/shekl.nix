ce966262b1a33d6da6d2545a6544dd81a2086f84
Evelyn <gaymario42069@gmail.com>
2024-12-11 13:47:54 -0500

    shekl.nix changed so rebuild wth proxy is a thing

--- home/linux/modules/shekl.nix
+++ home/linux/modules/shekl.nix
@@ -1,9 +1,10 @@
 {...}: {
-  programs.nushell.shellAliases = {
+  programs.nushell.shellAliases = rec {
     start-AA = "sudo systemctl start wg-quick-AirVPN-America.service";
     stop-AA = "sudo systemctl stop wg-quick-AirVPN-America.service";
     start-AP = "sudo systemctl start wg-quick-AirVPN-Phoenix.service";
     stop-AP = "sudo systemctl stop wg-quick-AirVPN-Phoenix.service";
     rb = "zsh -c \"nix fmt && find ~ -name '*.homemanagerbackup' -delete && nixos-rebuild switch --use-remote-sudo --flake .#default\"";
+    rbp = "proxychains4 -q -f proxychains.conf ${rb}";
   };
 }
