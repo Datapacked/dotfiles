29f1a1f37e091b3f073e823176cef20c48392c31
Evelyn <gaymario42069@gmail.com>
2024-12-12 16:12:14 -0500

    added and modified shell command aliases and added 16GB swap

--- home/linux/modules/shekl.nix
+++ home/linux/modules/shekl.nix
@@ -6,5 +6,6 @@
     stop-AP = "sudo systemctl stop wg-quick-AirVPN-Phoenix.service";
     rb = "zsh -c \"nix fmt && find ~ -name '*.homemanagerbackup' -delete && nixos-rebuild switch --use-remote-sudo --flake .#default\"";
     rbp = "proxychains4 -q -f proxychains.conf ${rb}";
+    sshocks = "ssh root@172.20.10.1 -N -D 1080";
   };
 }
